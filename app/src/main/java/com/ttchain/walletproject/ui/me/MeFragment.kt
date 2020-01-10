2e8ec59fff117f7aa7ec845cb11e9254427d9e82
Louis.Wu <zero761120@gmail.com>
2020-01-10 15:36:27 +0800

    更新 meViewModel inject

--- app/src/main/java/com/ttchain/walletproject/ui/me/MeFragment.kt
+++ app/src/main/java/com/ttchain/walletproject/ui/me/MeFragment.kt
@@ -1,5 +1,7 @@
 package com.ttchain.walletproject.ui.me
 
+import android.os.Bundle
+import android.view.View
 import com.ttchain.walletproject.R
 import com.ttchain.walletproject.base.BaseFragment
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -14,9 +16,16 @@ class MeFragment : BaseFragment() {
 
     override val layoutId = R.layout.fragment_setting
 
-    override fun initView() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+        initData()
     }
 
+    override fun initView() {
+    }
 
+    private fun initData() {
+
+    }
 }
