44db82d5e92fc769458ba19469070b906b89851a
Louis.Wu <zero761120@gmail.com>
2020-01-17 10:36:15 +0800

    修改 ContextWrapper 判斷和回傳的邏輯

--- app/src/main/java/com/ttchain/walletproject/ContextWrapper.kt
+++ app/src/main/java/com/ttchain/walletproject/ContextWrapper.kt
@@ -9,21 +9,25 @@ import java.util.Locale
 class ContextWrapper(base: Context) : android.content.ContextWrapper(base) {
     companion object {
 
-        fun wrap(context: Context, newLocale: Locale): Context {
+        fun wrap(context: Context?, newLocale: Locale): Context? {
 
-            val res = context.resources
-            val configuration = res.configuration
+            val res = context?.resources
+            val configuration = res?.configuration
             Locale.setDefault(newLocale)
 
             return when {
                 Build.VERSION.SDK_INT >= Build.VERSION_CODES.N -> {
-                    configuration.setLocale(newLocale)
-                    configuration.locales = LocaleList(newLocale)
-                    context.createConfigurationContext(configuration)
+                    configuration?.let {
+                        it.setLocale(newLocale)
+                        it.setLayoutDirection(newLocale)
+                        context.createConfigurationContext(configuration)
+                    } ?: run {
+                        context
+                    }
                 }
                 else -> {
-                    configuration.setLocale(newLocale)
-                    res.updateConfiguration(configuration, res.displayMetrics)
+                    configuration?.setLocale(newLocale)
+                    res?.updateConfiguration(configuration, res.displayMetrics)
                     context
                 }
             }
